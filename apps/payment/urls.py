1faf556a20c4cfd5b939799eec2eeeedceaa3bdc
Annderson Rey <anndersonrey@gamil.com>
2023-07-17 15:42:57 +1000

    modify .gitignore

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import GenerateTokenView, GetPaymentTotalView, ProcessPaymentView,CreatePaymentIntentView,StripeWebhookView
+from .views import GetPaymentTotalView,CreatePaymentIntentView,StripeWebhookView
 
 app_name="payment"
 
 urlpatterns = [
     path('get-payment-total', GetPaymentTotalView.as_view()),
-    path('get-token', GenerateTokenView.as_view()),
-    path('make-payment', ProcessPaymentView.as_view()),
     path('create-payment-intent', CreatePaymentIntentView.as_view()),
     path('stripe-webhook', StripeWebhookView, name='stripe-webhook'),
 ]
